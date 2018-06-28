248eba8bc3709da86bd1c24c74d0e4dc0da6c8bd
Shawn Shacterman <shawnshact@gmail.com>
2018-06-28 11:19:45 -0700

    fixes

--- src/opeNNDD_dataset.py
+++ src/opeNNDD_dataset.py
@@ -8,20 +8,22 @@ class OpeNNDD_Dataset:
     """data stats"""
     channels = 3 #num of channel for each image
     classes = 1 #num of classifications will be one since we want continuous output
-    total_train_ligands = 0 #placeholders for the real values
-    total_val_ligands = 0
-    total_test_ligands = 0
+    grid_dim = 72
 
     #instantiate with the hdf5 file and the train_batch_size of your choice
     def __init__(self, data_file, train_batch_size):
         assert os.path.exists(data_file), 'file does not exist' #make sure the path to the specified file exists
-        self.data_file = tb.open_file(data_file, mode='r') #handle to file
+        self.hdf5_file = tb.open_file(data_file, mode='r') #handle to file
+        self.total_train_ligands = len(self.hdf5_file.root.train_ligands) #placeholders for the real values
+        self.total_val_ligands = len(self.hdf5_file.root.val_ligands)
+        self.total_test_ligands = len(self.hdf5_file.root.test_ligands)
         self.train_indices = list(range(self.total_train_ligands)) #[0,total_train_ligands)... will be used later to shuffle the data between epochs and when loading initial batch if necessary
         self.val_indices = list(range(self.total_val_ligands)) #[0,total_val_ligands)
         self.test_indices = list(range(self.total_test_ligands)) #[0,total_test_ligands)
         self.train_batch_size = train_batch_size #training batch size for getting next batch in the dataset
         self.total_train_steps = self.total_train_ligands / train_batch_size #total amount of steps in a single epoch dependent on the batch size
         self.train_ligands_processed = 0
+
 
     def shuffle_train_data(self):
         random.shuffle(self.train_indices)
@@ -35,7 +37,7 @@ class OpeNNDD_Dataset:
 
     def next_train_batch(self):
         batch_size = self.train_batch_size
-        batch_ligands = np.zeros([batch_size, self.grid_dim, self.grid_dim, self.grid_dim, self.num_channels])
+        batch_ligands = np.zeros([batch_size, self.grid_dim, self.grid_dim, self.grid_dim, self.channels])
         batch_energies = np.zeros([batch_size])
         #get the next batch
         if (self.total_train_ligands - self.train_ligands_processed) < self.train_batch_size:
@@ -53,7 +55,7 @@ class OpeNNDD_Dataset:
 
 
     def val_set(self):
-        batch_ligands = np.zeros([self.total_val_ligands, self.grid_dim, self.grid_dim, self.grid_dim, self.num_channels])
+        batch_ligands = np.zeros([self.total_val_ligands, self.grid_dim, self.grid_dim, self.grid_dim, self.channels])
         batch_energies = np.zeros([self.total_val_ligands])
         for i in self.val_indices:
             batch_ligands[i] = self.hdf5_file.root.train_ligands[self.val_indices[i]]
@@ -64,7 +66,7 @@ class OpeNNDD_Dataset:
         return np.array(batch_ligands, dtype=np.float32), np.array(batch_energies, dtype=np.float32)
 
     def test_set(self):
-        batch_ligands = np.zeros([self.total_test_ligands, self.grid_dim, self.grid_dim, self.grid_dim, self.num_channels])
+        batch_ligands = np.zeros([self.total_test_ligands, self.grid_dim, self.grid_dim, self.grid_dim, self.channels])
         batch_energies = np.zeros([self.total_test_ligands])
         for i in self.test_indices:
             batch_ligands[i] = self.hdf5_file.root.train_ligands[self.test_indices[i]]
