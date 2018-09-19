8003c4cc11a7f2241d4c6fb16318defb416308ad
Michael Ganzorig <manlai1025@gmail.com>
2018-09-19 12:56:42 -0700

    modularizing

--- src/dataset_sampler.py
+++ src/dataset_sampler.py
@@ -7,24 +7,28 @@ import sys
 import shutil
 import random
 
-assert (len(sys.argv) == 4), 'Must input 3 inputs.'
-  
-""" inputs """
-n = int(sys.argv[1])    # number of poses to sample
-dir1 = str(sys.argv[2])     # entire dataset directory
-dir2 = str(sys.argv[3])     # destination directory for dataset sample
+"""
+n = number of poses to sample
+dir1 = entire dataset directory
+dir2 = destination directory for dataset sample
+"""
 
-""" getting n sdf files from the entire dataset """
-files = []
-for filename in os.listdir(dir1):
-    if filename[0] != '.' and not os.path.isdir(filename[0]) and filename.split('.')[1] == 'sdf':
-        files.append(filename)
+def sampler(n, dir1, dir2):
+
+    """ getting n sdf files from the entire dataset """
+    files = []
+    for filename in os.listdir(dir1):
+        if filename[0] != '.' and not os.path.isdir(filename[0]) and filename.split('.')[1] == 'sdf':
+            files.append(filename)
 
 
-assert (n <= len(files)), 'Cannot request more files than in directory.'
+    assert (n <= len(files)), 'Cannot request more files than in directory.'
 
-random.shuffle(files)
+    random.shuffle(files)
 
-""" copying sample dataset to destination directory """
-for i in range(n):
-    shutil.copyfile(os.path.join(dir1, files[i]) , os.path.join(dir2, files[i]))
+    """ copying sample dataset to destination directory """
+    for i in range(n):
+        shutil.copyfile(os.path.join(dir1, files[i]) , os.path.join(dir2, files[i]))
+
+if __name__ == "__main__":
+    sampler(int(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]))
